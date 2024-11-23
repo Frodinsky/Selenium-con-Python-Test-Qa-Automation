17f4faa389fbb59953b3a8f1efa16459b44935ab
frodinsky <rodolara97@gmail.com>
2024-11-22 20:27:26 -0700

    Commit initial

---
+++ test_selector_id.py
@@ -0,0 +1,24 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+# Crear las instancias de los navegadores manualmente
+chrome = webdriver.Chrome()
+
+chrome.get("https://demoqa.com/text-box")
+chrome.maximize_window()
+
+#primera extracion de un xpath
+extraccion_xpath = chrome.find_element(By.CSS_SELECTOR, "#userName")
+extraccion_xpath.send_keys("Holas")
+chrome.find_element(By.CSS_SELECTOR, "#userEmail").send_keys("pruebas@mail.com")
+chrome.find_element(By.CSS_SELECTOR, "#currentAddress").send_keys(f"Pruebias {extraccion_xpath}")
+chrome.find_element(By.CSS_SELECTOR, "#permanentAddress").send_keys("Pues sin novedades")
+chrome.execute_script("window.scrollTo(0,500)")
+time.sleep(1)
+chrome.find_element(By.CSS_SELECTOR, "#submit").click()
+
+time.sleep(3)
+
+chrome.close()
--- test_selector_xpath.py
+++ test_selector_xpath.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 # Crear las instancias de los navegadores manualmente
-#firefox = webdriver.Firefox()
 chrome = webdriver.Chrome()
 
 chrome.get("https://demoqa.com/text-box")

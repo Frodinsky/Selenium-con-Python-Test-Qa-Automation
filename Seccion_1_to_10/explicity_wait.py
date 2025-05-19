b06dd618135d4aec713e408c94fabfbba67bff48
frodinsky <rodolara97@gmail.com>
2025-05-19 03:18:41 -0700

    Aprendiendo a utilizar el explicty wait

--- Seccion_1_to_10/explicity_wait.py
+++ Seccion_1_to_10/explicity_wait.py
@@ -1,15 +1,28 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # Crear las instancias de los navegadores manualmente
 chrome = webdriver.Chrome()
-chrome.maximize_window()
-chrome.get("https://demoqa.com/text-box")
-tiempo = 1
+#chrome.maximize_window()
+chrome.get("https://validaciones.rodrigovillanueva.com.mx/Tiempos_Ok.html")
+
+espera = WebDriverWait(chrome, timeout=10)
+espera.until(EC.visibility_of_element_located((By.ID, "field1")))
+
+chrome.find_element(By.ID, "field1").send_keys("Rodolfo")
+chrome.find_element(By.ID, "field2").send_keys("Lara")
+combobox = chrome.find_element(By.ID, "comboBox")
+#Se ocupa algo como nombre de la variante porque no se que poner, pregunte a chatgpt y me dio cosas en ingles
+algo = Select(combobox)
+algo.select_by_index(2)
+chrome.find_element(By.ID, "checkbox").click()
+chrome.find_element(By.CSS_SELECTOR, ".btn.btn-primary").click()
+
+time.sleep(3)
 
 
-
-
-time.sleep(tiempo)
 chrome.close()

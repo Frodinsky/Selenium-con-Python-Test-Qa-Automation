9c5eec7e54c7a84f4c66a1b567636d0b0b003419
frodinsky <rodolara97@gmail.com>
2025-05-20 15:56:37 -0700

    Empezando con unitest

--- Seccion_11_to_19/empezando_unittest.py
+++ Seccion_11_to_19/empezando_unittest.py
@@ -13,10 +13,10 @@ class BaseTest(unittest.TestCase):
 
     def test1(self):
         self.driver.get("https://validaciones.rodrigovillanueva.com.mx/Campos_Uno_OK.html")
-        time.sleep(t)  # Para fines visuales solamente
 
     def tearDown(self):  # ✅ Se ejecuta al final del test
         self.driver.quit()  # Mejor que .close() porque cierra toda la sesión
+        time.sleep(t)  # Para fines visuales solamente
 
 # ✅ Esto debe ir fuera de la clase
 if __name__ == '__main__':
